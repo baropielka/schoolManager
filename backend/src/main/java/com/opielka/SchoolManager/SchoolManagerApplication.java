3caf0413a4d652554a9aa62daa74c8bddc9994fb
Bartek <najaneeciv@gmail.com>
2024-09-28 01:36:23 +0200

    Initial commit

---
+++ backend/src/main/java/com/opielka/SchoolManager/SchoolManagerApplication.java
@@ -0,0 +1,13 @@
+package com.opielka.SchoolManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagerApplication.class, args);
+	}
+
+}
